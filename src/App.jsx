517bb2288dd386cfa51e28b6a3015e1bd223c205
Kehinde Tobi <68740749+kentobias132@users.noreply.github.com>
2024-07-08 09:19:58 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import MainView from "./components/MainView";
 import PageView from "./components/PageView";
 import ProductDetail from "./components/ProductDetail";
 import ProductView from "./components/ProductView";
-import useScrollToTop from './useScrollToTop';
+// import useScrollToTop from './useScrollToTop';
 
 const appRouter = createBrowserRouter([
   {
@@ -36,7 +36,7 @@ const appRouter = createBrowserRouter([
 
 const App = () => {
   const { cart } = useContext(CartContext);
-  useScrollToTop();
+  // useScrollToTop();
 
   return (
     <div>
